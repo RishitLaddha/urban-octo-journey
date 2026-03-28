1fd370a8ba04f371edc05d949a1e019f0285e2d0
RishitLaddha <rladofficiel00@gmail.com>
2026-03-28 09:54:41 +0530

    Personalize UI message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Running on azure 🚀"
+    return "Running on azure 🚀 CI/CD done by Rishit Laddha 2309575"
 
 if __name__ == "__main__":
     app.run()
